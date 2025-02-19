98e1c6a069d648c18050c57e510e0ce68e0ff5a9
unknown <rochyatistat@gmail.com>
2025-02-19 21:02:36 +0700

    add dataframe

--- survival_rate_prediction.py
+++ survival_rate_prediction.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import pickle
 import streamlit as st
-from imblearn.under_sampling import RandomUnderSampler
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 
@@ -65,6 +64,12 @@ model.fit(X_train_cleaned, y_train_cleaned)
 # Evaluasi model
 y_pred_valid = model.predict(X_valid)
 y_pred_test = model.predict(X_test)
+data = {'MAE' : [mean_absolute_error(y_valid, y_pred_valid),
+                 mean_absolute_error(y_test, y_pred_test)],
+        'MSE': [mean_squared_error(y_valid, y_pred_valid),
+                mean_squared_error(y_test, y_pred_test)],
+        'R-squared': [r2_score(y_valid, y_pred_valid), r2_score(y_test, y_pred_test)]}
+df = pd.DataFrame(data, index=['validation Performance','Test Performance'])
 
 print("Validation Performance:")
 print("MAE:", mean_absolute_error(y_valid, y_pred_valid))
@@ -100,3 +105,4 @@ if st.sidebar.button("Predict Survival Rate", help="Click this button to predict
     features = np.array([[total_seed, area, target_cultivation_day]])
     prediction = model.predict(features)
     st.write(f"Predicted Survival Rate: {prediction[0]:.2f}")
+    st.table(df)
